53987d1c5626f2ba37841a0018c1cf7f14e29dcf
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-11-02 16:01:53 +0000

    Installed jQuery, Bootstrap, popper.js, FontAwesome

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2022_11_02_132637) do
     t.string "employee_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login_name"], name: "index_users_on_login_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
