8aced3771e0e98c4cb15798990bb8a44d5caaed2
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-11-02 13:33:18 +0000

    Generate models 'User' and 'Message'

---
+++ db/migrate/20221102132402_create_messages.rb
@@ -0,0 +1,15 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.integer :user_id, null: false
+      t.integer :last_update_user_id
+      t.string :title, null: false
+      #t.text :body
+      t.boolean :confirmation_flag, default: false
+      t.boolean :is_commentable, default: true
+      t.integer :number_of_comments, default: 0
+      t.datetime :update_content_at
+      t.timestamps
+    end
+  end
+end
