4d962f1d6d70f90c94f5224c1d47d2f0188f92f8
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-11-28 11:38:34 +0000

    flash変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,6 +53,7 @@ class MessagesController < ApplicationController
       end
       #送信者は自動的に受信者・編集者に追加
       message.message_destinations.create(receiver_id: current_user.id, is_editable: true)
+      flash[:noticce] = "メッセージを作成しました。"
       redirect_to message_path(message.id)
     end
 
@@ -146,7 +147,7 @@ class MessagesController < ApplicationController
         end
       end
 
-      flash[:notice] = "変更しました。"
+      flash[:notice] = "メッセージを変更しました。"
       redirect_to message_path(message.id)
     end
   end
