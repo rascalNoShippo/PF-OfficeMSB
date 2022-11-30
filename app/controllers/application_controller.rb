15458116ef2370790d9bc5d2413421f9e7ba104f
EC2 Default User <ec2-user@ip-172-31-34-53.ap-northeast-1.compute.internal>
2022-11-30 01:49:12 +0000

    sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :user_invalid
 	before_action :authenticate_user!
 	before_action :set_current_user
-	before_action :user_invalid
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:login_name, :email])
