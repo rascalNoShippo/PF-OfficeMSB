a5b42341823dddcf35c6a0e2cdde30fa1861ec75
EC2 Default User <naofirefox@gmail.com>
2022-12-02 07:14:32 +0000

    application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
+ before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :user_invalid
 	before_action :authenticate_user!
 	before_action :set_current_user
